619e89d16046bc8ed54039366cd39e3577f07f70
917tapoimarius <marius.tapoi@stud.ubbcluj.ro>
2023-11-10 14:03:11 +0200

    lab4-Documentation done

---
+++ lab4/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //MyScanner myScanner = new MyScanner("../lab1a/p1.txt");
+        //MyScanner myScanner = new MyScanner("../lab1a/p2.txt");
+        MyScanner myScanner = new MyScanner("../lab1a/p3.txt");
+        //MyScanner myScanner = new MyScanner("../lab1a/p3err.txt");
+        myScanner.scan();
+    }
+}
